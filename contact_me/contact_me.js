c33c5c624b5f996946276e2de2f26d5583d72ee2
MarioKuzmanov <googlemario1@abv.bg>
2024-11-25 19:12:26 +0100

    sketch new page

---
+++ contact_me/contact_me.js
@@ -0,0 +1,9 @@
+function onLoad() {
+    document.getElementById("go_back").addEventListener("click", function () {
+        document.getElementById("go_back").onclick = function () {
+            location.href = "../index.html";
+        }
+    })
+}
+
+window.addEventListener("load", onLoad)
---
+++ index.js
@@ -0,0 +1,28 @@
+function onClick() {
+
+    document.getElementById("contact_me").onclick = function () {
+        location.href = "contact_me/contact_me.html";
+    }
+
+    document.getElementById("work_education").onclick = function () {
+        location.href = "https://www.youtube.com/";
+    }
+
+    document.getElementById("projects").onclick = function () {
+        location.href = "https://www.youtube.com/";
+    }
+
+    document.getElementById("skills").onclick = function () {
+        location.href = "https://www.youtube.com/";
+    }
+}
+
+function onLoad() {
+    document.getElementById("contact_me").addEventListener("click", onClick);
+    document.getElementById("work_education").addEventListener("click", onClick);
+    document.getElementById("projects").addEventListener("click", onClick);
+    document.getElementById("skills").addEventListener("click", onClick);
+}
+
+
+window.addEventListener("load", onLoad)
