8eec8709ae0ea38480d13d14df9c4c6e674d5c05
MarioKuzmanov <googlemario1@abv.bg>
2024-12-13 19:49:36 +0100

    skills re-formatted

--- skills/skills.js
+++ skills/skills.js
@@ -1,7 +1,15 @@
-window.addEventListener("load", function () {
-    document.getElementById("go_back").addEventListener("click", function () {
-        document.getElementById("go_back").onclick = function () {
-            location.href = "../index.html";
-        }
-    })
-})
+function onLoad() {
+    let x = document.getElementsByClassName("btn");
+
+
+    x[0].addEventListener("click", function () {
+        location.href = "../index.html";
+    });
+
+    x[1].addEventListener("click", function () {
+        location.href = "../contact_me/contact_me.html";
+    });
+
+}
+
+window.addEventListener("load", onLoad);
